d7f87bf2910b22f8d512f1bc594c0cf764f57806
Matt Conforti <mattconforti21@gmail.com>
2021-08-17 14:33:45 -0400

    player page opens after search

--- html/player_page.php
+++ html/player_page.php
@@ -1,7 +1,37 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Player View</title>
+        <!-- get player name above? using php maybe?? -->
+        <!-- Google Font API -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Serif+Caption&display=swap" rel="stylesheet">
+        <!---->
+        <link rel="stylesheet" type="text/css" href="../styles/style.css">
     </head>
     <body>
+        <?php
+            // include the header
+            include('../includes/header.php');
+        ?>
+        <hr>
+        <!-- grid here -->
+        <div id="grid_container">
+            <div class="grid_item">
+            </div>
+            <div class="grid_item">
+            </div>
+            <div class="grid_item">
+            </div>
+            <div class="grid_item">
+            </div>
+            <div class="grid_item">
+            </div>
+            <div class="grid_item">
+            </div>
+        </div>
     </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
 <header id="page_header">
-    <img id="nba_logo" src="images/nba_logo.png" alt="NBA Logo">
+    <img id="nba_logo" src="/nbaDatabase/images/nba_logo.png" alt="NBA Logo">
     <h1>NBA Database</h1>
 </header>
