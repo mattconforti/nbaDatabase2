dad5d375bd8a5f397c5c924fc40b34c06ff2ad3b
Matt Conforti <mattconforti21@gmail.com>
2021-08-23 14:03:06 -0400

    player-page implementation and spacing

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <header id="page_header">
     <img id="nba_logo" src="/nbaDatabase/images/nba_logo.png" alt="NBA Logo">
     <h1>NBA Database</h1>
+    <!-- some type of menu here?? - need space filled & nav menu can be useful -->
 </header>
