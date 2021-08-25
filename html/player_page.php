c52603a59e8bd00a907550b00e48d80d35ccfed9
Matt Conforti <mattconforti21@gmail.com>
2021-08-24 23:54:14 -0400

    player-item design implementation

--- html/player_page.php
+++ html/player_page.php
@@ -21,96 +21,103 @@
         <!-- grid here -->
         <div id="grid_container">
             <div id="player_item" class="grid_item">
-                <!-- need to make photo section & 2 info sections surrounding --> 
-                <!-- & style them as 3 boxes stacked horizontally next to eachother with gap -->
-                <img id="player_photo" src="../images/player_photo.png" alt="player headshot">
-                <h2 class="player_info">36 y/o</h2>
-                <h2 class="player_info">6'9"</h2>
-                <h2 class="player_info">SF</h2>
-                <h2></h2>
+                <div id="subgrid_container">
+                    <div id="team_info" class="subgrid_item">
+                        <!-- need to load these images based on the specific player and team. src will be variable -->
+                        <img id="team_logo" src="../images/lakers_logo.png" alt="logo of player's team">
+                    </div>
+                    <div id="player_photo_item" class="subgrid_item">
+                        <img id="player_photo" src="../images/player_photo.png" alt="player headshot">
+                    </div>
+                    <div id="player_info_item" class="subgrid_item">
+                        <h2 class="player_info">36 y/o</h2>
+                        <h2 class="player_info">6'9"</h2>
+                        <h2 class="player_info">SF</h2>
+                    </div>
+                </div>
             </div>
             <div id="season_stats" class="grid_item">
                 <h2>Season Stats</h2>
                 <hr class="stats_dividers">
-                <div id="subgrid_container">
-                    <div class="subgrid_item">
+                <div id="subgrid_container2">
+                    <div class="subgrid2_item">
                         <h3>Season:</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>PTS</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>MIN</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>AST</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>REB</h3>
                     </div>
 
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>FG%</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>3P%</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                         <h3>'20-'21</h3>
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
-                    <div class="subgrid_item">
+                    <div class="subgrid2_item">
                     </div>
                 </div>
             </div>
             <div id="career_stats" class="grid_item">
                 <h2>Career Stats</h2>
                 <hr class="stats_dividers">
-                <div id="subgrid2">
-                    <div class="subgrid2_item">
+                <div id="subgrid3">
+                    <div class="subgrid3_item">
                         <h3>Season:</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>PTS</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>MIN</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>AST</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>REB</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>FG%</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>3P%</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                         <h3>Career</h3>
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
-                    <div class="subgrid2_item">
+                    <div class="subgrid3_item">
                     </div>
                 </div>
             </div>
