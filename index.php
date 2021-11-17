b2c11cd97db7e7919b88bb258262af97745f8698
Matt Conforti <mattconforti21@gmail.com>
2021-11-17 17:16:37 -0500

    footer & circle loader work

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
                 <button id="search_button">
                     <img id="search_icon" src="images/search_icon.png" alt="search button icon">
                 </button>
+                <div id="circle_loader"></div>
             </div>
         </div>
         <?php
