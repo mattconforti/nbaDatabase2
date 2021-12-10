5d211d63c75aff4054fe69e33d1b4958de5e7a42
Matt Conforti <mattconforti21@gmail.com>
2021-12-10 14:50:43 -0500

    home-navigation link around header

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
-<header id="page_header">
-    <img id="nba_logo" src="/nbaDatabase/images/nba_logo.png" alt="NBA Logo">
-    <h1>NBA Database</h1>
-    <!-- some type of menu here?? - need space filled & nav menu can be useful -->
-</header>
-
-<!-- MAKE CLICKING ON THE HEADER TAKE U TO THE HOME PAGE!!!! especially on player page -->
+<a id="header_link" href="/nbaDatabase/index.php">
+    <header id="page_header">
+        <img id="nba_logo" src="/nbaDatabase/images/nba_logo.png" alt="NBA Logo">
+        <h1>NBA Database</h1>
+        <!-- some type of menu here?? - need space filled & nav menu can be useful -->
+    </header>
+</a>
