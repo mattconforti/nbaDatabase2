71251a55e6e8e6530216851e27028fe1c0d1d428
Matt Conforti <mattconforti21@gmail.com>
2021-12-24 03:22:32 -0500

    more elements in media query

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,6 @@
 <a id="header_link" href="/nbaDatabase/index.php">
     <header id="page_header">
-        <!---------------------"../images/nba_logo.png" alt="NBA Logo"> -->
-        <img id="nba_logo" src="/nbaDatabase/images/nba_logo.png" alt="NBA Logo">
+        <img id="nba_logo" src="../images/nba_logo.png" alt="NBA Logo">
         <h1>NBA Database</h1>
         <!-- some type of menu here?? - need space filled & nav menu can be useful -->
     </header>
